796adf2162597c5767bf6accb97b7c5da1a4d72f
igormoreiraalves <igor@biomainvestimentos.com>
2024-01-30 18:48:51 -0300

    CHORE: config setting.py

---
+++ app/config/setting.py
@@ -0,0 +1,19 @@
+import dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    # General Settings
+    LOG_LEVEL: str | int | None = 'INFO'
+    SWAGGER_ENABLED: bool = True
+
+    # Security
+    API_KEY: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
+
+
+dotenv.load_dotenv()
+setting = Settings()
