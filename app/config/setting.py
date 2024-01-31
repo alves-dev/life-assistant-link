77ecd9230049ff24814766ef3f6e0ee67d87e25e
igormoreiraalves <igor@biomainvestimentos.com>
2024-01-31 12:55:50 -0300

    CHORE: add var PERSON_UUIDS

--- app/config/setting.py
+++ app/config/setting.py
@@ -10,6 +10,9 @@ class Settings(BaseSettings):
     # Security
     API_KEY: str
 
+    # HA
+    PERSON_UUIDS: dict
+
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
