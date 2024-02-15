8897acc5775f0125beafc0dee98ad5f8f54f61f0
igormoreiraalves <igor@biomainvestimentos.com>
2024-02-15 09:29:34 -0300

    FIX: connection

--- app/infrastructure/broker.py
+++ app/infrastructure/broker.py
@@ -1,21 +1,21 @@
-from threading import Thread
-import pika
 import json
+
+import pika
 from app.config.setting import setting
 
 connection_parameters = pika.ConnectionParameters(
-            host=setting.BROKER_HOST,
-            port=setting.BROKER_PORT,
-            credentials=pika.PlainCredentials(setting.BROKER_USERNAME, setting.BROKER_PASSWORD)
-        )
-connection = pika.BlockingConnection(connection_parameters)
+    host=setting.BROKER_HOST,
+    port=setting.BROKER_PORT,
+    credentials=pika.PlainCredentials(setting.BROKER_USERNAME, setting.BROKER_PASSWORD)
+)
 
 
 class RabbitMQ():
     def __init__(self):
         self.exchange = setting.BROKER_EXCHANGE
         self.routing_key = setting.BROKER_ROUTING_KEY
-        self.channel = connection.channel()
+        self.connection = pika.BlockingConnection(connection_parameters)
+        self.channel = self.connection.channel()
 
     def send(self, message: dict):
         self.channel.basic_publish(exchange=self.exchange, routing_key=self.routing_key, body=json.dumps(message))
