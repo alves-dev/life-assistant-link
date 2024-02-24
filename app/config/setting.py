43351eedb35d6bb2e927292821e80b0aed3e06a6
igormoreiraalves <igor@biomainvestimentos.com>
2024-02-24 10:44:56 -0300

    CI: config test

--- app/config/setting.py
+++ app/config/setting.py
@@ -12,15 +12,15 @@ class Settings(BaseSettings):
     API_KEY: str = uuid.uuid4().__str__()
 
     # HA
-    PERSON_UUIDS: dict
+    PERSON_UUIDS: dict = {}
 
     # broker RabbitMQ
-    BROKER_HOST: str
-    BROKER_PORT: int
-    BROKER_USERNAME: str
-    BROKER_PASSWORD: str
-    BROKER_EXCHANGE: str
-    BROKER_ROUTING_KEY: str
+    BROKER_HOST: str = 'localhost'
+    BROKER_PORT: int = 5672
+    BROKER_USERNAME: str = 'username'
+    BROKER_PASSWORD: str = 'password'
+    BROKER_EXCHANGE: str = 'exchange'
+    BROKER_ROUTING_KEY: str = 'routing-key'
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
